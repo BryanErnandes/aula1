bfee434af2adb28c1cc292e4a115eaed5a40fc4c
BryanErnandes <bryandeduartina1999@outlook.com>
2022-09-12 16:52:56 -0300

    curriculo

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  View, Text, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <StatusBar backgroundColor={'#000000'} barStyle='light-content' translucent={false}/>
+      <View tyle={styles.container}>
+        <View style={styles.Title}>
+          <Text style={styles.texTitle}>Aulas</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  Title: {
+    alignItems: 'center',
+  },
+  texTitle: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: '#B700FA',
+  }
+})
